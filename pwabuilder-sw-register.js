fce868478a9945acf019f6ca004c7435f7d23434
david rashid <rashid.david12@gmail.com>
2019-07-25 15:35:12 +0300

    service worker

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -1,4 +1,4 @@
-// This is the "Offline page" service worker
+// This is the service worker with the Advanced caching
 
 // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 
